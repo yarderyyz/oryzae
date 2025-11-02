a3ab11ab8486409815a5835f7e88b3e8a3d220c7
Lee Gauthier <lee.d.gauthier@gmail.com>
2025-11-02 15:29:02 +0100

    Remove allocation from Series processing fn

--- src/routing/series.rs
+++ src/routing/series.rs
@@ -1,40 +1,43 @@
 use crate::network::Network;
 
 /// Series routing component
-/// 
+///
 /// Chains multiple networks sequentially, where the output of one network
 /// becomes the input to the next network in the chain.
 pub struct Series {
     sub_networks: Vec<Box<dyn Network<f64> + Send>>,
-    out_frame: Vec<f64>,
+    out_frame: [f64; 32],
 }
 
 impl Series {
     /// Create a new series routing component
-    /// 
+    ///
     /// # Arguments
     /// * `networks` - Vector of networks to chain in series
-    /// 
+    ///
     /// # Returns
     /// * Boxed Series instance
     pub fn new(networks: Vec<Box<dyn Network<f64> + Send>>) -> Box<Self> {
         Box::new(Self {
             sub_networks: networks,
-            out_frame: vec![0.0],
+            out_frame: [0.0; 32],
         })
     }
 }
 
 impl Network<f64> for Series {
     fn get_frame(&mut self, in_frame: &[f64]) -> &[f64] {
-        let out_frame = self
+        let network_result = self
             .sub_networks
             .iter_mut()
             .fold(in_frame, |acc_frame, network| network.get_frame(acc_frame));
-        if self.out_frame.len() != out_frame.len() {
-            self.out_frame.resize(out_frame.len(), 0.0)
+
+        if network_result.len() > 32 {
+            panic!("Exceeded max of 32 channels");
         }
-        self.out_frame.copy_from_slice(out_frame);
-        self.out_frame.as_slice()
+
+        self.out_frame[0..network_result.len()].copy_from_slice(network_result);
+        &self.out_frame[0..network_result.len()]
     }
 }
+
